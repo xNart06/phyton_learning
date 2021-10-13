458cffa2b2fc27ec463c6073881294fae2d43bea
Furkan Hamza Bolat <70775462+xNart06@users.noreply.github.com>
2021-10-13 22:04:20 +0300

    Pyhon_ders3
    Operatöler

---
+++ baslangic_3.py
@@ -0,0 +1,21 @@
+# Python Boolen = True/False verisi
+a = 100
+b = 50
+if b > a:
+    print("b, büyüktür")
+else:
+    print("a, büyüktür")
+
+#__Python Operatörleri
+
+# + toplama // - çıkarma // * çarpma // / bölme // % mod // ** üs // "//"" kök 
+
+# x=5 // x+=3 x=x+3 // x-=3 x=x-3 // x /= 3	x = x / 3 // x %= 3  x = x % 3
+
+# == eşit != eşit değil 
+
+# ve= x<5 and x<10 // veya= x<5 or x<4// ifadenin değili= not(x<3 and x<10)
+
+# seri beilirtilen değeri içeriyosa "in" x in y
+
+# seri beilirtilen değeri içeriyosa "not in" x not in y
