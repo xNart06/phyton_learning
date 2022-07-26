f5edd73ecc26b2ab55b22fcdcdd4daa88beff82a
Furkan Hamza Bolat <70775462+xNart06@users.noreply.github.com>
2022-07-26 18:56:56 +0300

    Add files via upload

---
+++ Concatenating_Data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 26 18:56:04 2022
+
+@author: furka
+"""
+
+import pandas as pd 
+
+dictionary = {"NAME":["ali","veli","kenan","hilal","ayse","evren"],
+              "AGE":[15,16,17,33,45,66],
+              "MAAS":[100,150,240,350,110,220]}
+
+dataFrame1 = pd.DataFrame(dictionary)
