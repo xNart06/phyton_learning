4112c3822c93d14c31ea59d9e3479408e326c3bd
Furkan Hamza Bolat <70775462+xNart06@users.noreply.github.com>
2022-07-26 19:09:45 +0300

    Add files via upload

--- Concatenating_Data.py
+++ Concatenating_Data.py
@@ -12,3 +12,20 @@ dictionary = {"NAME":["ali","veli","kenan","hilal","ayse","evren"],
               "MAAS":[100,150,240,350,110,220]}
 
 dataFrame1 = pd.DataFrame(dictionary)
+
+dataFrame1.drop(["NAME"],axis=1,inplace=True) #tamamen yok etmek için inplace = True kullanıyoruz
+# axis=0 satır drop etme axis=1 sütun drop eder
+
+#2 data birleştirme
+data1 = dataFrame1.head()
+data2 = dataFrame1.tail()
+
+#vertical
+data_concat = pd.concat([data1,data2],axis=0)
+
+#horizontal
+maas = dataFrame1.MAAS
+age = dataFrame1.AGE
+data_h_concat = pd.concat([maas,age],axis=1)
+
+#drop ve concat gördük
